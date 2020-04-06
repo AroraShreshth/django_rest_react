ecf7a96a0450a0e9cfafb05247c18199acfdda70
Shreshth Arora <sarora_be18@thapar.edu>
2020-04-06 23:38:51 +0530

    solved issue

--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,10 +1,9 @@
+
 from rest_framework import serializers
-from .models import Lead
+from leads.models import Lead 
 
-#Lead Searlizer
-class LeadSerialzer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Lead
-        fields = '__all__'
-
+# Lead Serializer
+class LeadSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Lead 
+    fields = '__all__'
--- leadmanager/leads/urls.py
+++ leadmanager/leads/urls.py
@@ -1,7 +1,8 @@
-from rest_framework import routers 
+
+from rest_framework import routers
 from .api import LeadViewSet
 
 router = routers.DefaultRouter()
 router.register('api/leads', LeadViewSet, 'leads')
 
-urlpattern = router.urls
+urlpatterns = router.urls
