f158f3fe3733f24df18bb3581e41f90cff0637b7
Shreshth Arora <sarora_be18@thapar.edu>
2020-04-07 03:37:54 +0530

    Some React basic Architecture

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './layout/Header';
-
+import Dashboard from './leads/Dashboard';
 class App extends Component {
     render() {
         return (
-            <Header />
-            
+            <Fragment>
+                <Header />
+                <div class="container">
+                    <Dashboard />
+                </div>
+            </Fragment>
+
         )
     }
 }
