fd6edc5433bb48a4a12cfa921f3bc08bc09564c5
Shreshth Arora <sarora_be18@thapar.edu>
2020-04-07 12:08:24 +0530

    working leads display

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -26,13 +26,21 @@ export class Leads extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.leads.map(leads => (
+                        {this.props.leads.map((lead) => (
                             <tr key={lead.id}>
                                 <td>{lead.id}</td>
                                 <td>{lead.name}</td>
                                 <td>{lead.email}</td>
                                 <td>{lead.message}</td>
-                                <td><button className="btn btn-danger btn-sm">Delete</button></td>
+                                <td>
+                                    <button
+                                        // onClick={this.props.deleteLead.bind(this, lead.id)}
+                                        className="btn btn-danger btn-sm"
+                                    >
+                                        {' '}
+                                    Delete
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
