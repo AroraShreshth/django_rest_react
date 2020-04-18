f1c34b83134f999f19f7b1431b96c67c988d0992
Shreshth Arora <sarora_be18@thapar.edu>
2020-04-18 07:12:14 +0530

    functional error message

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,21 +1,35 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
 import Header from './layout/Header';
+import Alerts from './layout/Alerts';
 import Dashboard from './leads/Dashboard';
 import store from '../store';
 import { Provider } from 'react-redux';
 
+//Alert Options
+const alertOptions = {
+    timeout: 3000,
+    postion: "top center"
+
+}
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className="container">
-                        <Dashboard />
-                    </div>
-                </Fragment>
+                <AlertProvider template={AlertTemplate} {...alertOptions}>
+                    <Fragment>
+                        <Header />
+                        <Alerts />
+                        <div className="container">
+                            <Dashboard />
+                        </div>
+                    </Fragment>
+                </AlertProvider>
             </Provider>
         )
     }
