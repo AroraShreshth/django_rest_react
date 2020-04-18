8d2093e066788eec5fad6c775ed82da24ec1a576
Shreshth Arora <sarora_be18@thapar.edu>
2020-04-18 22:40:25 +0530

    register API, Login APi, Logout API working

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':
-    ('knox.auth.TokenAuthentication')
+    ['knox.auth.TokenAuthentication', ]
 }
 
 MIDDLEWARE = [
--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls')), # Frontend should load before any application !
-    path('', include('leads.urls'))
+    # Frontend should load before any application !
+    path('', include('frontend.urls')),
+    path('', include('leads.urls')),
+    path('', include('accounts.urls')),
 ]
